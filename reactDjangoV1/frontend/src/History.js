fa5b21394db8f0ae8426f451adca970d197f4235
nat997 <bibu880@gmail.com>
2024-06-22 11:42:58 +0200

    update css history

--- reactDjangoV1/frontend/src/History.js
+++ reactDjangoV1/frontend/src/History.js
@@ -1,67 +1,106 @@
-import React, { useEffect, useState } from 'react';
-import axiosInstance from './axiosConfig';
+import React, { useState } from "react";
+import axiosInstance from "./axiosConfig";
+import { loadStripe } from "@stripe/stripe-js";
 
-const History = () => {
-  const [history, setHistory] = useState([]);
-  const [loading, setLoading] = useState(true);
+import './Shop.css';
 
-  useEffect(() => {
-    const fetchHistory = async () => {
-      try {
-        const token = localStorage.getItem('token'); // Récupérer le token de localStorage
-        if (!token) {
-          alert('You need to log in first');
-          return;
-        }
+const stripePromise = loadStripe(
+  "pk_test_51LPtT9LIqjQ7Sl29dC93bDHkuaBVwN73rmQNeJPyQL2vnRAZ1TjYk9y4L8w9RmEV50mYKDUbDx0KLXWZRODi2XMg00Kg8uq8on"
+);
 
-        const response = await axiosInstance.get('/api/history/', {
+const Shop = ({ updateTickets }) => {
+  const [tickets, setTickets] = useState(0);
+
+  const handleBuyTickets = async (tickets) => {
+    const token = localStorage.getItem("token"); // Récupérer le token de localStorage
+    if (!token) {
+      alert("You need to log in first");
+      return;
+    }
+
+    try {
+      const response = await axiosInstance.post(
+        "/api/create_checkout_session",
+        { tickets: parseInt(tickets, 10) }, // Convertir en entier
+        {
           headers: {
-            'Authorization': `Token ${token}`
-          }
-        });
+            Authorization: `Token ${token}`,
+          },
+        }
+      );
 
-        setHistory(response.data);
-      } catch (error) {
-        console.error('Failed to fetch history:', error);
-      } finally {
-        setLoading(false);
+      const sessionId = response.data.sessionId;
+      const stripe = await stripePromise;
+      const { error } = await stripe.redirectToCheckout({ sessionId });
+      if (error) {
+        console.error("Stripe Checkout Error: ", error);
+        alert("Achat échoué : " + error.message);
       }
-    };
+    } catch (error) {
+      alert(
+        "Achat échoué: " +
+          (error.response ? error.response.data.message : error.message)
+      );
+    }
+  };
 
-    fetchHistory();
-  }, []);
+  const handleBuyCustomTickets = () => {
+    const token = localStorage.getItem("token");
+    if (!token) {
+      alert("You need to log in first");
+      return;
+    }
 
-  if (loading) {
-    return <div>Loading...</div>;
-  }
+    axiosInstance.post("/api/buy_tickets", { tickets: parseInt(tickets, 10) }, {
+      headers: {
+        'Authorization': `Token ${token}`
+      }
+    })
+    .then(response => {
+      alert('Tickets purchased successfully');
+      updateTickets(response.data.tickets); // Mettre à jour les tickets après l'achat
+    })
+    .catch(error => {
+      alert('Purchase failed: ' + (error.response ? error.response.data : error.message));
+    });
+  };
 
   return (
-    <div className="history-container">
-      <h1>Historique des Scraps</h1>
-      <table>
-        <thead>
-          <tr>
-            <th>Ville</th>
-            <th>Magasin</th>
-            <th>Tickets en Cours</th>
-            <th>Type de Scrap</th>
-            <th>Date</th>
-          </tr>
-        </thead>
-        <tbody>
-          {history.map((entry, index) => (
-            <tr key={index}>
-              <td>{entry.ville}</td>
-              <td>{entry.magasin}</td>
-              <td>{entry.nb_ticket_en_cours}</td>
-              <td>{entry.type_scrap}</td>
-              <td>{new Date(entry.created_at).toLocaleString()}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+    <div className="shop-container">
+      <h1>Acheter des tickets</h1>
+      <div className="shop-cards">
+        <div className="shop-card">
+          <h2>5 Tickets</h2>
+          <p>Testez les versions de scraping medium et premium avec 5 tickets.</p>
+          <div className="price">4.99€</div>
+          <button onClick={() => handleBuyTickets(5)}>Acheter</button>
+        </div>
+        <div className="shop-card">
+          <h2>15 Tickets</h2>
+          <p>Testez les versions de scraping medium et premium avec 15 tickets.</p>
+          <div className="price">14.99€</div>
+          <button onClick={() => handleBuyTickets(15)}>Acheter</button>
+        </div>
+        <div className="shop-card">
+          <h2>30 Tickets</h2>
+          <p>Testez les versions de scraping medium et premium avec 30 tickets.</p>
+          <div className="price">29.99€</div>
+          <button onClick={() => handleBuyTickets(30)}>Acheter</button>
+        </div>
+      </div>
+      <div className="ticket-purchase">
+        <h2>Buy Custom Number of Tickets</h2>
+        <input
+          type="number"
+          value={tickets}
+          onChange={e => setTickets(e.target.value)}
+          placeholder="Number of tickets"
+          min="1"
+        />
+        <button onClick={handleBuyCustomTickets}>Buy Tickets</button>
+      </div>
     </div>
   );
 };
 
-export default History;
+export default Shop;
